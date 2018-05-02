f301527c8c87d5543e918751e943c09983714a56
Chris Knific <chris@mutations.ltd>
2018-05-01 21:42:32 -0700

    Update Hero styles

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -94,7 +94,7 @@ const Bio = () => (
       <BioContainer>
         <h4>Bio</h4>
         <p>Virgil is an instrumental progressive rock outfit based out of Los Angeles, CA. "The Pacer" is the band's debut EP and features Brandon Giffin on bass, Justin McKinney on lead guitar, and Navene Koperweis on drums.</p>
-        <p>Piloting the group is Chris Knific, who draws on influences such as Pat Matheny, Yes, Cynic, and Devin Townsend, resulting in a mixture of fusion jazz, prog rock, and metal.</p>
+        <p>Piloting the group is Chris Knific, who draws on influences such as Pat Metheny, Yes, Cynic, and Devin Townsend, resulting in a mixture of fusion jazz, prog rock, and metal.</p>
         <p>While Knific has scored films and written both popular and academic work, "The Pacer" is Knific's first exercise in his journey to meld these musical worlds with his own artistic vision.</p>
       </BioContainer>
     </Container>
