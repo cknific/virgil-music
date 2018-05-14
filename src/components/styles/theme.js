3042dbb52acf19ac0783ab1d1006eef12a729edc
Chris Knific <chris@mutations.ltd>
2018-05-13 17:54:15 -0700

    Add ThemeProvider and theme.js

---
+++ src/components/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+  // Bootstrap breakpoint sizes
+  breakpoints: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+  },
+
+  colors: {
+    white: '#fff',
+  },
+}
